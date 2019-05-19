b13e3f9e713552602dd1cb3fa5160544cb6efc1a
Cesar Domingos <cesar_web@live.com>
2019-05-19 10:15:10 -0300

    Creating Pie Chart

--- main.py
+++ main.py
@@ -9,21 +9,27 @@ planilha.set_column('B:B', 20)
 
 bold = excel_file.add_format({'bold': True})
 
-planilha.write(0, 0, "FRUTAS", bold)
-planilha.write(0, 1, "LEGUMES", bold)
+# Creating DataSet to create chart
+header = ['CATEGORY', 'VALUES']
+data = [['Maca', 'Uva', 'Pera', 'Morango'], [30, 13, 17, 40]]
 
-planilha.write("A2", "Maca")
-planilha.write("B2", "Cenoura")
+planilha.write_row('A1', header, bold)
+planilha.write_column('A2', data[0])
+planilha.write_column('B2', data[1])
 
-planilha.write("A3", "Pera")
-planilha.write("B3", "Batata")
+# Creating Chart
+chart = excel_file.add_chart({'type': 'pie'})
+chart.add_series({
+    'name': 'Fruits liked',
+    'categories': ['Sheet1', 1, 0, 4, 0],
+    'values': ['Sheet1', 1, 1, 4, 1]
+})
+chart.set_title({'name': 'My fruits most liked'})
+chart.set_style(10)
 
-planilha.write("A4", "Morango")
-planilha.write("B4", "Cebola")
+planilha.insert_chart('D1', chart, {'x_offset': 20, 'y_offset': 25})
 
-planilha.write("A5", "Limao")
-planilha.write("B5", "Beringela")
-
-planilha.insert_image("D1", "logo.png")
+# Adding image to plan
+planilha.insert_image("A20", "logo.png")
 
 excel_file.close()
